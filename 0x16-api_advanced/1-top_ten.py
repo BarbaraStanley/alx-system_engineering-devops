681c882450136f63bec7041229325d25e15cb15f
BarbaraStanley <barbeaboo@gmail.com>
2023-02-01 00:09:07 +0000

    trying luck

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,11 +3,12 @@
 
 import requests
 
+
 def top_ten(subreddit):
     """ function that queries api and prints titles of first 10 hot posts"""
 
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    params = {"limit":10}
+    params = {"limit": 10}
     result = requests.get(url, params=params, allow_redirects=False)
 
     if result.status_code == 404:
