dc122bacc2c349449f19e83a677b6164bcafa011
BarbaraStanley <barbeaboo@gmail.com>
2023-03-09 22:20:41 +0000

    test or final

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,8 @@
+package { 'nginx': ensure => installed }
+file { '/etc/default/nginx':
+  ensure  => file,
+  content => 'ULIMIT="-n 4096"\n',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+service { 'nginx': ensure => running, enable => true, require => File['/etc/default/nginx'] }i
